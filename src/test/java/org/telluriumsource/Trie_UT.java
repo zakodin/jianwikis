5f84de54d461b40dcc17f6cb4cbcf2df611e0ac5
John.Jian.Fang <John.Jian.Fang@450ea352-5969-11de-a527-d564076fe1eb>
2009-12-22 16:43:45 +0000

    randomize input

--- src/test/java/org/telluriumsource/Trie_UT.java
+++ src/test/java/org/telluriumsource/Trie_UT.java
@@ -3,6 +3,10 @@ package org.telluriumsource;
 import org.junit.Test;
 import static org.junit.Assert.assertNotNull;
 
+import java.util.Random;
+import java.util.List;
+import java.util.ArrayList;
+
 
 /**
  * @author Jian Fang (John.Jian.Fang@gmail.com)
@@ -11,11 +15,31 @@ import static org.junit.Assert.assertNotNull;
  */
 public class Trie_UT {
 
+    public String[] randomize(String[] dictionary){
+        if(dictionary != null && dictionary.length > 0){
+            Random rand = new Random();
+            List<String> list = new ArrayList<String>();
+            for(String str: dictionary){
+                list.add(str);
+            }
+            List<String> nlist = new ArrayList<String>();
+            while(list.size() > 0){
+                int index = rand.nextInt(list.size());
+                nlist.add(list.remove(index));
+            }
+
+            return nlist.toArray(new String[0]);
+        }
+
+        return null;
+    }
+    
     @Test
     public void testInsert(){
         String[] dictionary = {"a", "an", "and", "andy", "bo", "body", "bodyguard", "some", "someday", "goodluck", "joke"};
         Trie trie = new Trie();
-        trie.buildTree(dictionary);
+        String[] ndict = randomize(dictionary);
+        trie.buildTree(ndict);
         trie.checkAndIndexLevel();
         trie.printMe();
         Node deepest = trie.getDeepestNode();
